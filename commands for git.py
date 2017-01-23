72ce1173fe9e48ea90da7435847329fd878560c1
chrisjoy15 <chris.r.joy@gmail.com>
2017-01-23 15:11:10 -0500

    commands

---
+++ commands for git.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 23 15:06:56 2017
+
+@author: Chris
+"""
+
+cd " changes directory"
+cd .. goes backwards
+
+ls lists whats in the current directory (dir for window)
+
+git clone " brings a repository to my computer"
+git status " checks files in repository if anything is different
+git commit -m "add message" (prepares a file to send to repository)
+git push " sends file"
+git pull " grabs files that may have been updated
